97e9fbfa63b550567cddc80ba5e446b848f59ed0
Debasis_sahu <sahurocky524@gmail.com>
2025-06-09 23:02:51 +0530

    service reponsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
     <>
       <Hero />
       <Navbar />
-      {/* <Service /> */}
+      <Service />
       {/* <Value /> */}
       {/* <Portfolio /> */}
       {/* <Faqs /> */}
